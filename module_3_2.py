59c2d3aa16022a959d4498a170fe37a729fdd0b3
ivanartemiev <art.lx.ru@gmail.com>
2024-12-08 20:28:27 +0300

    Update module_3_2.py

--- module_3_2.py
+++ module_3_2.py
@@ -1,9 +1,13 @@
-def send_email(message, recipient, sender='university.help@gmail.com'):
-    a = recipient[-3:]
-    b = sender[-3:]
+def send_email(message, recipient, sender = 'university.help@gmail.com'):
+    a = 0
+    b = 0
+    if recipient.find('.ru') > 0 or recipient.find('.com') > 0 or recipient.find('.net') > 0:
+        a = 1
+    if sender.find('.ru') > 0  or sender.find('.com') > 0 or sender.find('.net') > 0:
+        b = 1
     c = recipient.find('@')
     d = sender.find("@")
-    if (c >= 0 and d >= 0) and (a == '.ru' or a == 'com' or a == 'net') and (b == '.ru' or b == 'com' or b == 'net'):
+    if (c > 0 and d > 0) and a > 0 and b > 0:
         if recipient == sender:
             print('Нельзя отправить письмо самому себе!')
         elif sender == 'university.help@gmail.com':
@@ -13,6 +17,6 @@ def send_email(message, recipient, sender='university.help@gmail.com'):
     else:
         print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
 send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
-send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
-send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
-send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender = 'urban.info@gmail.com')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender = 'urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender = 'urban.teacher@mail.ru')
